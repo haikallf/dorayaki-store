7b8924187a144e978aec7894879fa3c1406dbc2c
Farhan <ahandnr@gmail.com>
2021-11-25 23:46:07 +0700

    rest request

--- check/soap-req.php
+++ check/soap-req.php
@@ -1,13 +1,36 @@
 <?php 
+session_start();
+if (!isset($_SESSION['username'])) {
+
+    header("Location: ../pages/login.php");
+    exit;
+    return;
+
+}
 
 try {
+    $sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+    socket_connect($sock, "8.8.8.8", 53);
+    socket_getsockname($sock, $name);
+    // local machine's external IP address
+    $localAddr = strval($name);
+
+    $id = 1;
+    $qty = 2;
+
     $soapclient = new SoapClient('http://localhost:8081/ws/req?wsdl');
-    $response = $soapclient->RequestDorayakiPabrik();
-    
-    // ini pesan konfirmasi request berhasil atau tidak
-    $soapreq = json_decode(json_encode($response), true);
-    var_dump($soapreq);
-    
+    $param = new stdClass();
+    $param->arg0 = $localAddr;
+    $param->arg1 = $_SESSION['username'];
+    $param->arg2 = $id;
+    $param->arg3 = $qty;
+    $response = $soapclient->RequestDorayakiPabrik($param);
+
+    $soapreqdorayaki = json_decode(json_encode($response), true);
+    var_dump($soapreqdorayaki);
+    var_dump($soapreqdorayaki["return"]);
+   
+
 } catch (Exception $e) {
     echo $e->getMessage();
 }
